eb3d63edcf0b1e2f9b62d9ca6e52f77ea093ca9e
Grombuggs <alex_koski@hotmail.com>
2025-08-07 21:29:32 +0300

    Small tweaks.

--- lib/screens/fi_subscription.dart
+++ lib/screens/fi_subscription.dart
@@ -1,34 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:in_app_purchase/in_app_purchase.dart';
+import 'package:affirmation_forest/utils/subscription.dart';
 
-class FiSubscriptionPage extends StatelessWidget {
-  final List<ProductDetails> products;
+class FiSubscriptionPage extends StatefulWidget {
+	  const FiSubscriptionPage({Key? key}) : super(key: key);
+  @override
+  _SubscriptionPageState createState() => _SubscriptionPageState();
+}
 
-  FiSubscriptionPage({required this.products});
+class _SubscriptionPageState extends State<FiSubscriptionPage> {
+  final SubscriptionService _subscriptionService = SubscriptionService();
+
+  @override
+  void initState() {
+    super.initState();
+    _initialize();
+  }
+
+  Future<void> _initialize() async {
+    await _subscriptionService.initialize();
+    await _subscriptionService.fetchProducts();
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
+    final products = _subscriptionService.products;
+
     return Scaffold(
-      appBar: AppBar(title: Text('Subscriptions')),
-      body: ListView.builder(
-        itemCount: products.length,
-        itemBuilder: (context, index) {
-          final product = products[index];
-          return ListTile(
-            title: Text(product.title),
-            subtitle: Text(product.description),
-            trailing: TextButton(
-              onPressed: () {
-                // Purchase the subscription
-                InAppPurchase.instance.buyNonConsumable(
-                  purchaseParam: PurchaseParam(productDetails: product),
+      appBar: AppBar(title: Text('Subscription')),
+      body: products.isEmpty
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: products.length,
+              itemBuilder: (context, index) {
+                final product = products[index];
+                return ListTile(
+                  title: Text(product.title),
+                  subtitle: Text(product.description),
+                  trailing: TextButton(
+                    onPressed: () => _subscriptionService.buySubscription(product),
+                    child: Text(product.price),
+                  ),
                 );
               },
-              child: Text(product.price),
             ),
-          );
-        },
-      ),
     );
   }
 }
