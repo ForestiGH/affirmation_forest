76890ae3ad8a29a391a2858ee1947b347ea8a0d5
Grombuggs <alex_koski@hotmail.com>
2025-09-01 15:19:46 +0300

    Started swipe feature.

--- lib/screens/affirmation_AA_En.dart
+++ lib/screens/affirmation_AA_En.dart
@@ -1,9 +1,32 @@
 import 'package:affirmation_forest/utils/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:affirmation_forest/utils/affirmation_display.dart';
+import 'package:flutter_card_swiper/flutter_card_swiper.dart';
 
 class AffirmationAAEn extends StatelessWidget {
-const AffirmationAAEn({super.key});
+	List<Container> cards = [
+	Container(
+		alignment: Alignment.center,
+		child: AffirmationDisplay(
+			categoryName: 'Balanced_and_fulfilling_everyday_life',
+			jsonAssetPath: 'assets/sheets/affirmations.json',
+			),
+		),
+	Container(
+		alignment: Alignment.center,
+		child: AffirmationDisplay(
+			categoryName: 'Balanced_and_fulfilling_everyday_life',
+			jsonAssetPath: 'assets/sheets/affirmations.json',
+			),
+	),
+	Container(
+		alignment: Alignment.center,
+		child: AffirmationDisplay(
+			categoryName: 'Balanced_and_fulfilling_everyday_life',
+			jsonAssetPath: 'assets/sheets/affirmations.json',
+			),
+	),
+	];
 
 @override
 Widget build(BuildContext context) {
@@ -11,13 +34,12 @@ return Scaffold(
 	appBar: AppBar(
 		backgroundColor: MyColor.bianca,
 		),
-	body: Padding(
-		padding: const EdgeInsets.all(16.0),
-		child: AffirmationDisplay(
-			categoryName: 'Balanced_and_fulfilling_everyday_life',
-			jsonAssetPath: 'assets/sheets/affirmations.json',
+	body: Flexible(
+		child: CardSwiper(
+			cardsCount: cards.length,
+			cardBuilder: (context, index, percentThresholdX, percentThresholdY) => cards[index],
+			),
 		),
-	),
-);
+	);
 }
 }
